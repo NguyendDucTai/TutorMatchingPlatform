514a3245c35af815b9a0687cf306996a92110a28
NguyendDucTai <nguyenductai08102004@gmail.com>
2026-07-21 18:40:13 +0700

    Add Register Tutor Profile feature

---
+++ TutorMatchingPlatform.Application/Auth/Commands/SetupTutorProfile/SubjectRateDto.cs
@@ -0,0 +1,8 @@
+namespace TutorMatchingPlatform.Application.Auth.Commands.SetupTutorProfile
+{
+    public class SubjectRateDto
+    {
+        public int SubjectId { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
